0145fa78a0f1885e2ac7c49c799ced19e3edf0b3
Allan White <allan@catalyze.io>
2018-07-09 18:24:08 -0700

    Got cookie behavior where we want it.

---
+++ source/assets/js/webinar.js
@@ -0,0 +1,30 @@
+$("document").ready(function(){
+    var webinarId = $('#webinar').data('webinar');
+    function showWebinar(){
+        $('#webinar-play').removeClass("hide");
+        $('#webinar-preview').addClass("hide");
+        $('#report-form').addClass("hide");
+        console.log('show it');
+    }
+    function hideWebinar(){
+        $('#webinar-play').addClass("hide");
+        $('#webinar-preview').removeClass("hide");
+        $('#report-form').removeClass("hide");
+        console.log('default');
+    }
+    if($.cookie('webinarReg') == null){
+        // console.log('no match. hide thing');
+        hideWebinar();
+    } else if($.cookie('webinarReg') == webinarId) {
+        // console.log('match! show thing');
+        showWebinar();
+    }
+    $('body').on('hsvalidatedsubmit', '.hs-form', function (e) {
+        // set cookie to ID 
+        $.cookie('webinarReg', webinarId, { expires: 120 });
+        // var form = $(e.target);    
+        // var email = form.find('[name=email]').val();
+        // $.cookie(webinarCookie, 'true', { expires: 30 });
+        showWebinar();
+    });
+});
